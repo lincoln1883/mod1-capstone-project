6bf97e795b7d31b703387b2dfc7df952c63b85a4
lincoln gibson <lincolngibson7@gmail.com>
2023-04-19 01:34:14 -0300

    Add interactivity to mobile menu using javascript.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const mobileMenu = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+const openMenu = document.querySelector('#menu-open');
+const closeMenu = document.querySelector('#menu-close');
+
+openMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu');
+  document.body.style.position = 'fixed';
+});
+
+closeMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu');
+  document.body.style.position = '';
+});
+
+navLinks.forEach((element) => {
+  element.addEventListener('click', () => {
+    mobileMenu.classList.remove('mobile-menu');
+    document.body.style.position = '';
+  });
+});
